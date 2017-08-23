c613fab36a03dc1af77e04c0cfceeb9cd83e17b7
Spencer Westwood <spencer.westwood@aerian.com>
2017-08-23 10:41:22 +0100

    Models created
    Course, Lesson
    Auth scaffolded
    Migrations
    Resource Controllers

---
+++ database/migrations/2017_08_23_093756_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('image');
+            $table->text('description');
+            $table->datetime('start_date');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
